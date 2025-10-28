d26e194bc4711cbc8e0601944b7053f807f0fc0f
this <this@thisui-MacBookPro.local>
2025-10-28 10:28:49 +0900

    미니퀘스트 - CORSMiddleware

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import logging
 from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
 from starlette.middleware.base import BaseHTTPMiddleware
+from starlette.middleware.cors import CORSMiddleware
 import time
 from typing import Dict, Deque
 from collections import deque
@@ -10,7 +11,6 @@ from collections import deque
 # 로거 설정
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
-
 
 class TimeoutMiddleware(BaseHTTPMiddleware):
     def __init__(self, app, timeout: int = 10):
@@ -35,12 +35,11 @@ class TimeoutMiddleware(BaseHTTPMiddleware):
 
 rate_limit_data: Dict[str, Deque[float]] = {}
 
-
 class RateLimitMiddleware(BaseHTTPMiddleware):
     def __init__(self, app, requests_limit: int = 5, time_window: int = 10):
         super().__init__(app)
         self.requests_limit = requests_limit  # 허용 요청 횟수
-        self.time_window = time_window  # 시간 창 (초)
+        self.time_window = time_window      # 시간 창 (초)
 
     async def dispatch(self, request: Request, call_next):
         client_ip = request.client.host if request.client else "unknown"
@@ -57,7 +56,7 @@ class RateLimitMiddleware(BaseHTTPMiddleware):
         if len(request_timestamps) >= self.requests_limit:
             return JSONResponse(
                 {"detail": "Too Many Requests"},
-                status_code=429  # 429 Too Many Requests
+                status_code=429 # 429 Too Many Requests
             )
 
         # 현재 요청의 타임스탬프 추가
@@ -67,8 +66,14 @@ class RateLimitMiddleware(BaseHTTPMiddleware):
         response = await call_next(request)
         return response
 
-
 app = FastAPI()
+
+ALLOW_ORIGINS = [
+    "http://localhost:8080",
+    "http://127.0.0.1:8080",
+    "http://localhost:8000",
+    "http://127.0.0.1:8000",
+]
 
 app.add_middleware(TimeoutMiddleware, timeout=15)
 
@@ -77,3 +82,13 @@ app.add_middleware(
     requests_limit=5,
     time_window=10
 )
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=ALLOW_ORIGINS,
+    allow_credentials=False,
+    allow_methods=["*"],
+    allow_headers=["*"],
+    expose_headers=["*"],
+)
+
