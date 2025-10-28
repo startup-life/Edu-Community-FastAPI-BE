95aad23fb0643aa9d5e8b6083390100a34a0d82c
this <this@thisui-MacBookPro.local>
2025-10-28 16:52:13 +0900

    주석 추가

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from fastapi import FastAPI, Request
 
 app = FastAPI()
 
+# POST 요청 예제
+# Request Body에서 user_name과 age를 받아서 인사 메시지를 반환하는 엔드포인트
 @app.post("/user/init")
 async def user_init(request: Request):
     body = await request.json()
