fd418ad11e5f931516d5172a1eee21d232caa673
this <this@thisui-MacBookPro.local>
2025-10-28 10:20:52 +0900

    과제- fastAPI post 요청

--- main.py
+++ main.py
@@ -1,24 +1,15 @@
-from fastapi import FastAPI
-from fastapi.responses import JSONResponse
+from fastapi import FastAPI, Request
+
 
 app = FastAPI()
 
-@app.get("/customstatus")
-def create_item():
-    content = {"item_id": 2, "name": "수동 생성"}
-    # 상태 코드를 201로 지정하여 응답
-    return JSONResponse(content=content, status_code=201)
-
-@app.get("/customheaders")
-def get_with_headers():
-    content = {"message": "헤더 확인!"}
-    headers = {"X-Request-ID": "abc-123", "Cache-Control": "no-cache"}
-    # headers 딕셔너리를 함께 전달
-    return JSONResponse(content=content, headers=headers)
-
-@app.get("/cookie")
-def login():
-    resp = JSONResponse(content={"message": "로그인 성공"})
-    # 응답 객체에 쿠키를 설정
-    resp.set_cookie(key="session_id", value="user1234", httponly=True)
-    return resp
+@app.post("/user/init")
+async def user_init(request: Request):
+    body = await request.json()
+    user_name = body.get("user_name")
+    age = body.get("age")
+    return {
+        "user_name": user_name,
+        "age": age,
+        "message": f"안녕하세요! {user_name}님!",
+    }
