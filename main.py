72108deca430179a80c72442b42776ca85dd8aa9
this <this@thisui-MacBookPro.local>
2025-10-28 16:47:49 +0900

    주석 추가

--- main.py
+++ main.py
@@ -3,12 +3,18 @@ from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
+# 실무에서 많이 사용되는 JSONResponse를 활용하여 구현
+# JSONResponse를 많이 사용하는 이유는 상태 코드, 헤더, 쿠키 등을 세밀하게 제어할 수 있고
+# FastAPI의 기본 응답 클래스인 Response를 상속받아 구현되어 있어 호환성이 좋고 자동 JSON 직렬화 기능을 제공해서 편리
+
+# 커스텀 상태 코드 지정
 @app.get("/customstatus")
 def create_item():
     content = {"item_id": 2, "name": "수동 생성"}
     # 상태 코드를 201로 지정하여 응답
     return JSONResponse(content=content, status_code=201)
 
+# 커스텀 헤더 추가
 @app.get("/customheaders")
 def get_with_headers():
     content = {"message": "헤더 확인!"}
@@ -16,6 +22,7 @@ def get_with_headers():
     # headers 딕셔너리를 함께 전달
     return JSONResponse(content=content, headers=headers)
 
+# 쿠키 설정
 @app.get("/cookie")
 def login():
     resp = JSONResponse(content={"message": "로그인 성공"})
